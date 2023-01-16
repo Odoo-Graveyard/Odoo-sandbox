c5553968af988bf615c37545a63d540f993b65ef
Hinamizawa <hinamizawa.github@gmail.com>
2023-01-16 13:52:41 -0400

    up

---
+++ local-addons/my_library/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "My library",
+ 'summary': "Manage books easily",
+ 'description': """ 
+ Manage Library
+ ==============
+ Description related to library.
+ """, 
+ 'author': "Hinamizawa", 
+ 'website': "http://www.example.com", 
+ 'category': 'Uncategorized', 
+ 'version': '14.0.1', 
+ 'depends': ['base'], 
+ 'data': ['views/views.xml'], 
+ 'demo': ['demo.xml'],
+ 'images': ['static/description/ah.webp'],
+}
