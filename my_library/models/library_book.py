881ed75c85f6f90ba19540fc18be1bc6f2aa92a8
Hinamizawa <hinamizawa.github@gmail.com>
2023-02-06 16:48:43 -0400

    p

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -12,6 +12,25 @@ class LibraryBook(models.Model):
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
     notepad = fields.Text('Internal Notes')
+    from odoo import models, fields 
+class LibraryBook(models.Model): 
+ # ... 
+ short_name = fields.Char('Short Title') 
+ notes = fields.Text('Internal Notes') 
+ state = fields.Selection( 
+ [('draft', 'Not Available'), 
+ ('available', 'Available'), 
+ ('lost', 'Lost')], 
+ 'State') 
+ description = fields.Html('Description') 
+ cover = fields.Binary('Book Cover') 
+ out_of_print = fields.Boolean('Out of Print?') 
+ date_release = fields.Date('Release Date') 
+ date_updated = fields.Datetime('Last Updated') 
+ pages = fields.Integer('Number of Pages') 
+ reader_rating = fields.Float('Reader Average Rating', digits=(14, 4), # Optional precision decimals,
+ ) 
+ 
 
 def name_get(self):
  result = []
