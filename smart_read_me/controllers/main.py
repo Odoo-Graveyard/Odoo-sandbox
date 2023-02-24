41133a05b8f0c4e539c77112d7ac6a12c3e05598
Hinamizawa <hinamizawa.github@gmail.com>
2023-02-24 12:16:21 -0400

    Update main.py

--- smart_read_me/controllers/main.py
+++ smart_read_me/controllers/main.py
@@ -3,4 +3,4 @@ from odoo import http
 class MyWebsite(http.Controller):
     @http.route('/home', type='http', auth="public", website=True)
     def my_page(self, **kw):
-        return http.request.render('my_module.home', {})
+        return http.request.render('website.home', {})
