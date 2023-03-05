9386f07364b76a97c78e526423af0d1c6a255f4f
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-05 14:49:34 -0400

    Update main.py

--- smart_read_me/controllers/main.py
+++ smart_read_me/controllers/main.py
@@ -3,4 +3,4 @@ from odoo import http
 class MyController(http.Controller):
     @http.route('/test', auth='public', website=True)
     def test_page(self, **kwargs):
-        return http.request.render('my_module.test_template', {})
+        return http.request.render('smart_read_me.test_template', {})
