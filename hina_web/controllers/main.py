d7e5c40360a30ef8aed20b8b11c7a587a4169025
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-05 15:35:02 -0400

    s

--- hina_web/controllers/main.py
+++ hina_web/controllers/main.py
@@ -1,6 +1,7 @@
 from odoo import http
 
+
 class MyController(http.Controller):
-    @http.route('/', auth='public', website=True)
+    @http.route('/test', auth='public', website=True)
     def home(self, **kw):
         return http.request.render('hina_web.home_page')
