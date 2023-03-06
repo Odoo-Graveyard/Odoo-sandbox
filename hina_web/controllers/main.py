0f242714209bfe89a23fc8bf8f964ef00b7617b3
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-05 20:21:53 -0400

    ssss

--- hina_web/controllers/main.py
+++ hina_web/controllers/main.py
@@ -5,3 +5,7 @@ class MyController(http.Controller):
     @http.route('/test', auth='public', website=True)
     def home(self, **kw):
         return http.request.render('hina_web.home_page')
+
+    # @http.route('/test2', auth='public', website=True)
+    # def home2(self, **kw):
+    #     return http.request.render('hina_web.home_page2')
